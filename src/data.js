caf1a3ee612acd16058ed0eebbeee4cc049958bd
Quan2003promax <anhquan.261103@gmail.com>
2025-11-12 01:41:01 +0700

    need cv

---
+++ src/data.js
@@ -0,0 +1,240 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/ai.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/laravel.png";
+import Tools12 from "/assets/tools/wordpress.png";
+import Tools13 from "/assets/tools/framer.png";
+import Tools14 from "/assets/tools/html.png";
+import Tools15 from "/assets/tools/css.png";
+import Tools16 from "/assets/tools/ts.png";
+import Tools17 from "/assets/tools/php.png";
+import Tools18 from "/assets/tools/vite.png";
+import Tools19 from "/assets/tools/mysql.png";
+
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Next JS",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Laravel",
+    ket: "Framework",
+    dad: "700",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "Wordpress",
+    ket: "CMS",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Adobe Photoshop",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "Framer",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "HTML",
+    ket: "Language",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1500",
+  },
+  {
+    id: 16,
+    gambar: Tools16,
+    nama: "TypeScript",
+    ket: "Language",
+    dad: "1600",
+  },
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "PHP",
+    ket: "Language",
+    dad: "1700",
+  },
+  {
+    id: 18,
+    gambar: Tools18,
+    nama: "Vite",
+    ket: "Framework",
+    dad: "1800",
+  },
+  {
+    id: 19,
+    gambar: Tools19,
+    nama: "MySql",
+    ket: "Framework",
+    dad: "1900",
+  },
+];
+
+import Anhquan1 from "/assets/anhquan/anhquan1.png";
+import Anhquan2 from "/assets/anhquan/anhquan2.png";
+import Anhquan3 from "/assets/anhquan/anhquan3.png";
+import Anhquan4 from "/assets/anhquan/anhquan4.png";
+import Anhquan5 from "/assets/anhquan/anhquan5.png";
+import Anhquan6 from "/assets/anhquan/anhquan6.png";
+
+export const listProyek = [
+  {
+    id: 1,
+    image: Anhquan1,
+    title: "Queue numbering system",
+    subtitle: "A queue numbering system organizes customer...",
+    fullDescription: "A queue numbering system organizes customer flow by issuing sequential tickets and calling them to service counters in an orderly fashion.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/Quan2003promax",
+    dad: "100",
+  },
+  {
+    id: 2,
+    image: Anhquan2,
+    title: "Website design with Framer Fanpage",
+    subtitle: "Designed and launched a dynamic and visually-driven fanpage...",
+    fullDescription: "Designed and launched a dynamic and visually-driven fanpage and e-commerce website using Framer, dedicated to the artist ROSÉ. The site features a modern, clean aesthetic with a strong emphasis on high-quality imagery and typography.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://rosesarerosiebyanhquan.framer.website/",
+    dad: "200",
+  },
+  {
+    id: 3,
+    image: Anhquan3,
+    title: "Tracking Application",
+    subtitle: "A full-stack web application built with React.js for the frontend ...",
+    fullDescription: "A full-stack web application built with React.js for the frontend for the backend. The project is structured to separate client-side and server-side logic, likely creating a RESTful API to manage data and a dynamic user interface to interact with it. The 'tracking' name suggests its purpose may involve monitoring data, user activity, or object locations in real-time.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/Quan2003promax/tracking_reacnode",
+    dad: "300",
+  },
+  {
+    id: 4,
+    image: Anhquan4,
+    title: "Personal Web Portfolio",
+    subtitle: "An interactive web portfolio showcasing my professional journey...",
+    fullDescription: "An interactive web portfolio showcasing my professional journey, skills, and projects. Designed with a clean yet modern aesthetic, the portfolio highlights my technical expertise in web development, software engineering, and IoT. The site also serves as a central hub for potential employers and collaborators to explore my works, reflecting both my creativity and technical precision.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://github.com/Quan2003promax",
+    dad: "400",
+  },
+  {
+    id: 5,
+    image: Anhquan5,
+    title: "Design Website",
+    subtitle: "Designed and developed xediencudaklak.com...",
+    fullDescription: "Designed and developed xediencudaklak.com, an e-commerce website for used electric vehicles, implementing intuitive product catalogs, detailed listings, and integrated multi-channel CTA buttons (Hotline, Zalo) to optimize user conversion and support SEO through a dedicated content section.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://xediencudaklak.com/xe-dap-dien-cu",
+    dad: "500",
+  },
+  {
+    id: 6,
+    image: Anhquan6,
+    title: "Wordpress Coffee Shop Website",
+    subtitle: "A fully functional website designed for a coffee shop, enabling...",
+    fullDescription: "A fully functional website designed for a coffee shop, enabling customers to explore the menu, learn about the shop’s story, and place orders online. The project focused on creating an attractive and modern interface, integrating responsive design for mobile and desktop, and ensuring smooth navigation for users. This website not only improved customer experience but also supported the coffee shop’s digital presence and business growth.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://github.com/Quan2003promax",
+    dad: "600",
+  },
+];
